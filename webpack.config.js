ff1e2131464dd5584aec771826d42964ca77178d
iMykhailychenko <igor.c.m@ukr.net>
2020-11-11 15:26:24 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,29 @@
-const webpack = require('webpack');
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'production',
+  entry: './src/index.jsx',
   optimization: {
     minimize: false,
   },
   output: {
+    path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
-  },
-  externals: {
-    react: {
-      root: 'React',
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-    },
-    'react-transition-group': {
-      root: ['ReactTransitionGroup'],
-      commonjs: 'react-transition-group',
-      commonjs2: 'react-transition-group',
-      amd: 'react-transition-group',
-    },
-    'prop-types': {
-      root: 'PropTypes',
-      commonjs: 'prop-types',
-      commonjs2: 'prop-types',
-      amd: 'prop-types',
-    },
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
+        use: [MiniCssExtractPlugin.loader, 'css-loader',],
+      }
     ],
   },
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.jsx', '.js'],
-  },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
     new MiniCssExtractPlugin({
       filename: 'index.css',
       chunkFilename: 'index.css',
